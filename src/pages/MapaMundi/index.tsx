6179c81d49a9f36ab3c3f027379766d3146c9465
RA4Z <rraz639@gmail.com>
2024-06-19 09:08:11 -0300

    Adicionando ícone da áfrica do sul

--- src/pages/MapaMundi/index.tsx
+++ src/pages/MapaMundi/index.tsx
@@ -2,6 +2,7 @@ import Mapa from 'assets/mapa-mundi.svg'
 import Brasil from 'assets/brasil.png'
 import India from 'assets/india.png'
 import USA from 'assets/estados-unidos.png'
+import Africa from 'assets/africa-do-sul.png'
 import styles from './MapaMundi.module.scss'
 
 export default function MapaMundi() {
@@ -12,6 +13,7 @@ export default function MapaMundi() {
                 <img src={Brasil} className={styles.countries__brazil} alt='Ícone do Brasil' />
                 <img src={India} className={styles.countries__india} alt='Ícone da Índia' />
                 <img src={USA} className={styles.countries__usa} alt='Ícone dos Estados Unidos' />
+                <img src={Africa} className={styles.countries__africa} alt='Ícone da África do Sul' />
             </div>
         </header>
     )
