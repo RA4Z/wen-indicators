63aca8d3077d666722b8bf778f35c700520ffef0
RA4Z <rraz639@gmail.com>
2024-06-20 11:24:12 -0300

    Exibindo pop up sobre execução automática ou manual

--- src/pages/MapaMundi/index.tsx
+++ src/pages/MapaMundi/index.tsx
@@ -5,8 +5,7 @@ import Indicador from 'pages/Indicador'
 import Logo from 'assets/logo.png'
 import MapChart from './MapChart'
 import { CaretRightOutlined, PauseOutlined } from '@ant-design/icons';
-
-import { Button, Tooltip } from 'antd';
+import { Button, Tooltip, message } from 'antd';
 
 export default function MapaMundi() {
     const [automatic, setAutomatic] = useState(false)
@@ -28,10 +27,14 @@ export default function MapaMundi() {
             <Indicador country={country} nome={indicador} isIndicadorOpen={isIndicadorOpen} setIsIndicadorOpen={setIsIndicadorOpen} />
             <MapChart selectCountry={selectCountry} />
             <img title='Projeto Desenvolvido e Prototipado por Robert Aron Zimmermann' src={Logo} className={styles.logo} alt='Ícone da África do Sul' />
+
             <Tooltip title={automatic ? 'Pausar Exibição' : 'Iniciar Exibição'} color='geekblue'>
                 <Button icon={automatic ? <PauseOutlined /> : <CaretRightOutlined />}
-                    className={styles.automatic} type="primary" shape="round"
-                    onClick={() => setAutomatic(!automatic)} size='large' />
+                    className={styles.automatic} type="primary" shape="round" size='large'
+                    onClick={() => {
+                        setAutomatic(!automatic);
+                        message.info(automatic ? 'Exibição Automática Pausada' : 'Exibição Automática Iniciada')
+                    }} />
             </Tooltip>
 
         </header>
