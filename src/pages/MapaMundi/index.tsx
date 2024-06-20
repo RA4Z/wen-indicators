e9a9ce05f1a1091cbf2092e2d211f4d51bc99c26
RA4Z <rraz639@gmail.com>
2024-06-20 11:14:38 -0300

    Criando botão para pausar e iniciar exibição automática

--- src/pages/MapaMundi/index.tsx
+++ src/pages/MapaMundi/index.tsx
@@ -4,14 +4,19 @@ import { useState } from 'react'
 import Indicador from 'pages/Indicador'
 import Logo from 'assets/logo.png'
 import MapChart from './MapChart'
+import { CaretRightOutlined, PauseOutlined } from '@ant-design/icons';
+
+import { Button, Tooltip } from 'antd';
 
 export default function MapaMundi() {
+    const [automatic, setAutomatic] = useState(false)
     const [country, setCountry] = useState('')
     const [indicador, setIndicador] = useState('')
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [isIndicadorOpen, setIsIndicadorOpen] = useState(false)
 
     function selectCountry(country: string) {
+        setAutomatic(false)
         setCountry(country)
         setIsModalOpen(true);
     }
@@ -23,6 +28,12 @@ export default function MapaMundi() {
             <Indicador country={country} nome={indicador} isIndicadorOpen={isIndicadorOpen} setIsIndicadorOpen={setIsIndicadorOpen} />
             <MapChart selectCountry={selectCountry} />
             <img title='Projeto Desenvolvido e Prototipado por Robert Aron Zimmermann' src={Logo} className={styles.logo} alt='Ícone da África do Sul' />
+            <Tooltip title={automatic ? 'Pausar Exibição' : 'Iniciar Exibição'} color='geekblue'>
+                <Button icon={automatic ? <PauseOutlined /> : <CaretRightOutlined />}
+                    className={styles.automatic} type="primary" shape="round"
+                    onClick={() => setAutomatic(!automatic)} size='large' />
+            </Tooltip>
+
         </header>
     )
 }
