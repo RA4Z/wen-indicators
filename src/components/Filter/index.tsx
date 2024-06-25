1084c84b86b42d74f9566984b088080ba5b9a13d
RA4Z <rraz639@gmail.com>
2024-06-25 14:33:49 -0300

    Mantendo dados somente do servidor

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -10,18 +10,18 @@ interface Props {
     setFiltros: any
 }
 
-export default function Filter(props: Props) {
+export default function Filter({ filtros, setFiltros }: Props) {
     const [open, setOpen] = useState(false)
     const [alterando, setAlterando] = useState(false)
 
     const onChange = (e: any, index: any) => {
-        const novosFiltros = [...props.filtros];
+        const novosFiltros = [...filtros];
         novosFiltros[index].selecionado = e.target.checked;
-        props.setFiltros(novosFiltros);
+        setFiltros(novosFiltros);
         setAlterando(true)
     };
 
-    const items: MenuProps['items'] = props.filtros.map((filtro: any, index: any) => ({
+    const items: MenuProps['items'] = filtros.map((filtro: any, index: any) => ({
         key: index.toString(), // Use o índice como chave
         label: (
             <Checkbox
@@ -34,19 +34,40 @@ export default function Filter(props: Props) {
 
     useEffect(() => {
         const info = localStorage.getItem('filtros_ativos');
+      
         if (info) {
-            const parsedInfo = JSON.parse(info);
-            props.setFiltros(parsedInfo);
+          const parsedInfo = JSON.parse(info);
+      
+          // Percorre cada filtro em 'filtros'
+          const filtrosAtualizados = filtros.map((filtro:any) => {
+            // Tenta encontrar um filtro correspondente em 'parsedInfo'
+            const infoFiltro = parsedInfo.find(
+              (infoItem:any) => infoItem.nome === filtro.nome
+            );
+      
+            // Se encontrar um filtro correspondente, atualiza 'selecionado'
+            if (infoFiltro) {
+              return {
+                ...filtro,
+                selecionado: infoFiltro.selecionado,
+              };
+            }
+      
+            // Caso contrário, mantém o filtro original
+            return filtro;
+          });
+      
+          setFiltros(filtrosAtualizados);
         }
-    }, []);
+      }, []);
 
     useEffect(() => {
         if (alterando) {
-            const itemsString = JSON.stringify(props.filtros);
+            const itemsString = JSON.stringify(filtros);
             localStorage.setItem('filtros_ativos', itemsString);
             setAlterando(false)
         }
-    }, [props.filtros, alterando])
+    }, [filtros, alterando])
 
     return (
         <>
