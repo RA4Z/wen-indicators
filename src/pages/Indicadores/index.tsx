d9a9069cdc0a5e436cd15f809032707984601fae
RA4Z <rraz639@gmail.com>
2024-07-02 10:06:43 -0300

    Adicionando bandeira no indicador

--- src/pages/Indicadores/index.tsx
+++ src/pages/Indicadores/index.tsx
@@ -3,6 +3,14 @@ import styles from './Indicador.module.scss'
 import { useEffect, useState } from 'react'
 import Tabela from './Tabela'
 
+import Alemanha from 'images/bandeira alemanha.png'
+import Africa from 'images/bandeira africa.png'
+import Brasil from 'images/bandeira brasil.png'
+import China from 'images/bandeira china.png'
+import India from 'images/bandeira india.png'
+import Portugal from 'images/bandeira portugal.png'
+import USA from 'images/bandeira usa.png'
+
 interface Props {
     country: string
     isModalOpen: boolean,
@@ -17,6 +25,7 @@ interface Props {
 
 export default function Indicadores(props: Props) {
     const [data, setData] = useState<any>()
+    const [countryImage, setCountryImage] = useState(Brasil)
 
     const handleOk = () => {
         props.setIsModalOpen(false);
@@ -27,6 +36,13 @@ export default function Indicadores(props: Props) {
     };
 
     useEffect(() => {
+        if (props.country === 'Germany') setCountryImage(Alemanha)
+        if (props.country === 'South Africa') setCountryImage(Africa)
+        if (props.country === 'Brazil') setCountryImage(Brasil)
+        if (props.country === 'China') setCountryImage(China)
+        if (props.country === 'India') setCountryImage(India)
+        if (props.country === 'Portugal') setCountryImage(Portugal)
+        if (props.country === 'United States') setCountryImage(USA)
         const value = props.indicators.filter((indicator: any) => indicator.id === props.country)
         if (value.length > 0) {
             const filtrados = props.filtros.filter((filtro: any) => filtro.selecionado === true)
@@ -59,7 +75,11 @@ export default function Indicadores(props: Props) {
     return (
         <>
             {data &&
-                <Modal width={'95vw'} title={`Indicadores ${data.country}`}
+                <Modal width={'95vw'} title={
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <img width={50} src={countryImage} alt={`${data.country} Flag`} />
+                        <span style={{ marginLeft: 10 }}>{`Indicadores ${data.country}`}</span>
+                    </div>}
                     className={styles.modal}
                     open={props.isModalOpen} onOk={handleOk} onCancel={handleCancel}
                     footer={[]}>
