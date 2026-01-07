d1348c245c7db58a8703585c7772b791ccb06ae3
Robert Aron Zimmermann <82897913+RA4Z@users.noreply.github.com>
2026-01-07 08:18:33 -0300

    Update new IP address

--- src/services/requisition.ts
+++ src/services/requisition.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export async function getIndicadores() {
     try {
-        const url = 'http://10.1.43.63:5000/WenIndicators/Indicators/Display';
+        const url = 'http://10.1.43.72:5000/WenIndicators/Indicators/Display';
         const response = await axios.get(url, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         });
@@ -21,7 +21,7 @@ export async function getIndicadores() {
 
 export async function getDatabase() {
     try {
-        const url = 'http://10.1.43.63:5000/WenIndicators/Database/Display';
+        const url = 'http://10.1.43.72:5000/WenIndicators/Database/Display';
         const response = await axios.get(url, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         });
@@ -40,7 +40,7 @@ export async function getDatabase() {
 
 export async function getResults() {
     try {
-        const url = 'http://10.1.43.63:5000/WenIndicators/Results/Display';
+        const url = 'http://10.1.43.72:5000/WenIndicators/Results/Display';
         const response = await axios.get(url, {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
         });
